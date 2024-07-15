c28053dfec41c39e10feeaa8da1dfc053954b208
ParlaknFX <ckerallan@gmail.com>
2024-07-15 23:43:35 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,5 +1,12 @@
-<template>
-  <main>
-    <h1>Welcome to Driftoverse, it's a test miiverse revival!</h1>
-  </main>
-</template>
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Driftoverse, it's a test miiverse revival!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
